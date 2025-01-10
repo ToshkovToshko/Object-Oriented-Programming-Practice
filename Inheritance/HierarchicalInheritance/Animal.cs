20242908390db7b549ddca2fb37d62e618d68a12
ToshkoToshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2025-01-10 23:32:52 +0200

    Hierarchical Inheritance Commit

---
+++ Inheritance/HierarchicalInheritance/Animal.cs
@@ -0,0 +1,10 @@
+﻿namespace HierarchicalInheritance
+{
+    public class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Eating...");
+        }
+    }
+}
---
+++ Inheritance/HierarchicalInheritance/StartUp.cs
@@ -0,0 +1,9 @@
+﻿using HierarchicalInheritance;
+
+Dog dog = new Dog();
+dog.Eat();
+dog.Bark();
+
+Cat cat = new Cat();
+cat.Eat();
+cat.Meow();

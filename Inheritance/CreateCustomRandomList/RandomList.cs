7d852581ea20ca79a9b6d192cf3d5708b81a44d4
ToshkoToshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2025-01-10 23:45:11 +0200

    Create Custom Random List Commit

---
+++ Inheritance/CreateCustomRandomList/RandomList.cs
@@ -0,0 +1,17 @@
+﻿namespace CreateCustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            random = new Random();
+        }
+
+        public string RandomString()
+        {
+            return this[random.Next(0, this.Count)];
+        }
+    }
+}
---
+++ Inheritance/CreateCustomRandomList/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using CreateCustomRandomList;
+
+RandomList list = new RandomList();
+
+list.Add("First");
+list.Add("Second");
+list.Add("Third");
+list.Add("Fourth");
+list.Add("Fifth");
+
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+
+list.Remove("Second");
+
+foreach (var item in list)
+{
+    Console.WriteLine(item);
+}
+
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(list.RandomString());
+}

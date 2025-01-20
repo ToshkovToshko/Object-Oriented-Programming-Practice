07312abae2a682be72a6283d222d1f3cbe004413
ToshkoToshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2025-01-20 20:19:39 +0200

    Players and monsters commit

---
+++ Inheritance/PlayersAndMonsters/StartUp.cs
@@ -0,0 +1,9 @@
+﻿using PlayersAndMonsters;
+
+string username = Console.ReadLine();
+int level = int.Parse(Console.ReadLine());
+
+
+SoulMaster soulMaster = new SoulMaster(username, level);
+
+Console.WriteLine(soulMaster.ToString());

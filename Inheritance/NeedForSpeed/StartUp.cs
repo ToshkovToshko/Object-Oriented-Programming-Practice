89e1f4c7c6a259b580351ee778b5efb2078e5bce
ToshkoToshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2025-01-20 20:54:48 +0200

    vehicles commit

---
+++ Inheritance/NeedForSpeed/StartUp.cs
@@ -0,0 +1,11 @@
+﻿using NeedForSpeed;
+
+int horsePower = int.Parse(Console.ReadLine());
+double fuel = double.Parse(Console.ReadLine());
+double kilometers = double.Parse(Console.ReadLine());
+
+RaceMotorcycle raceMoto = new RaceMotorcycle(horsePower, fuel);
+
+raceMoto.Drive(kilometers);
+
+Console.WriteLine(raceMoto.ToString());
---
+++ Inheritance/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,29 @@
+﻿namespace NeedForSpeed
+{
+    public class Vehicle
+    {
+        private double DefaultFuelConsumption = 1.25;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            this.HorsePower = horsePower;
+            this.Fuel = fuel;
+        }
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+
+        public double Fuel { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public virtual void Drive(double kilometers)
+        {
+            this.Fuel -= this.FuelConsumption * kilometers;
+        }
+
+        public override string ToString()
+        {
+            return $"Type: {this.GetType().Name}, HP: {this.HorsePower}, Fuel rest: {this.Fuel}";
+        }
+    }
+}

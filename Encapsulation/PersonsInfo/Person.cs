de21fa6c5893205932311cf6982ab95b497ac706
ToshkoToshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2025-01-21 16:50:26 +0200

    Encapsulation first commit

---
+++ Encapsulation/PersonsInfo/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string fistName, string lastName, int Age)
+        {
+            this.FirstName = fistName;
+            this.LastName = lastName;
+            this.Age = Age;
+        }
+
+        public string FirstName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public int Age { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} is {this.Age} years old.";
+        }
+
+    }
+}
---
+++ Encapsulation/PersonsInfo/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using PersonsInfo;
+
+int n = int.Parse(Console.ReadLine());
+
+List<Person> persons = new List<Person>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] input = Console.ReadLine().Split();
+
+    Person person = new Person(input[0], input[1], int.Parse(input[2]));
+    persons.Add(person);
+}
+
+persons.OrderBy(p => p.FirstName)
+       .ThenBy(p => p.Age)
+       .ToList()
+       .ForEach(p => Console.WriteLine(p.ToString()));

84112ce56c7d8769fd3f33591a8e726ec4f8bb68
ToshkoToshkov <87978077+ToshkoToshkov@users.noreply.github.com>
2025-02-18 09:08:52 +0200

    Dough class commit

--- Encapsulation/PizzaCalories/Dough.cs
+++ Encapsulation/PizzaCalories/Dough.cs
@@ -1,12 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PizzaCalories
+﻿namespace PizzaCalories
 {
     class Dough
     {
+        private const int minWeight = 1;
+        private const int maxWeight = 200;
+
+        private string flourType;
+        private string bakingTechnique;
+        private int weight;
+
+        public Dough(string flourType, string bakingtechnique, int weight)
+        {
+            this.FlourType = flourType;
+            this.BakingTechnique = bakingTechnique;
+            this.Weight = weight;
+        }
+
+        public string FlourType 
+        {
+            get => this.flourType;
+            private set
+            {
+                var valueAsLower = value.ToLower();
+
+                if (valueAsLower != "white" && valueAsLower != "wholegrain")
+                {
+                    throw new ArgumentException("Invalid type of dough!");
+                }
+
+                this.flourType = value;
+            }
+        }
+
+        public string BakingTechnique
+        {
+            get => this.bakingTechnique;
+            private set
+            {
+                var valueAsLower = value.ToLower();
+
+                if (valueAsLower != "crispy" && valueAsLower != "chewy" && valueAsLower != "homemade")
+                {
+                    throw new ArgumentException("Invalid baking technique!");
+                }
+
+                this.bakingTechnique = value;
+            }
+        }
+
+        public int Weight 
+        {
+            get => this.weight;
+            private set
+            {
+                if (value < minWeight || value > maxWeight)
+                {
+                    throw new ArgumentException($"Dough weight should be in the range [{minWeight}...{maxWeight}]!");
+                }
+
+                this.weight = value;
+            }
+        }
+
     }
 }

1961926b46380ae034fbcf49dac4bcec2504e678
toshkotoshkov <toshkotoshkov@abv.bg>
2025-08-02 14:37:01 +0300

    Robots info commit

---
+++ Interfaces-And-Abstraction/PersonInfo/IRobot.cs
@@ -0,0 +1,7 @@
+﻿namespace PersonInfo
+{
+    interface IRobot
+    {
+        string Model { get; set; }
+    }
+}
--- Interfaces-And-Abstraction/PersonInfo/StartUp.cs
+++ Interfaces-And-Abstraction/PersonInfo/StartUp.cs
@@ -1,14 +1,57 @@
 ﻿using PersonInfo;
 
-string name = Console.ReadLine();
-int age = int.Parse(Console.ReadLine());
-string id = Console.ReadLine();
-string birthdate = Console.ReadLine();
+string input = Console.ReadLine();
 
-IIdentifiable identifiable = new Citizen(name, age, id, birthdate);
-IBirthable birthable = new Citizen(name, age, id, birthdate);
+List<Citizen> citizens = new List<Citizen>();
+List<Robot> robots = new List<Robot>();
 
-Console.WriteLine(identifiable.Name);
-Console.WriteLine(identifiable.Age);
-Console.WriteLine(identifiable.Id);
-Console.WriteLine(birthable.Birthdate);
+while (input != "End")
+{
+    string[] data = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    if (data.Length == 4)
+    {
+        string name = data[0];
+        int age = int.Parse(data[1]);
+        string id = data[2];
+        string birthdate = data[3];
+
+        Citizen citizen = new Citizen(name, age, id, birthdate);
+        citizens.Add(citizen);
+    }
+    else
+    {
+        string model = data[0];
+        string id = data[1];
+
+        Robot robot = new Robot(model, id);
+        robots.Add(robot);
+    }
+
+    input = Console.ReadLine();
+}
+
+string fakeId = Console.ReadLine();
+
+List<string> fakeIds = new List<string>();
+
+foreach (var item in citizens)
+{
+    if (item.Id.EndsWith(fakeId))
+    {
+        fakeIds.Add(item.Id);
+    }
+}
+
+foreach (var item in robots)
+{
+    if (item.Id.EndsWith(fakeId))
+    {
+        fakeIds.Add(item.Id);
+    }
+}
+
+foreach (var item in fakeIds)
+{
+    Console.WriteLine(item);
+}

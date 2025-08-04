7c70509e2cda41f288a0dad1873760393f9b6800
toshkotoshkov <toshkotoshkov@abv.bg>
2025-08-04 21:10:48 +0300

    Polimorphism first commit

---
+++ Polymorphism/MathOperation/StartUp.cs
@@ -0,0 +1,6 @@
+﻿using MathOperation;
+
+Operations operation = new Operations();
+Console.WriteLine(operation.Add(2, 3));
+Console.WriteLine(operation.Add(2.2, 3.3, 5.5));
+Console.WriteLine(operation.Add(2.2m, 3.3m, 4.4m));

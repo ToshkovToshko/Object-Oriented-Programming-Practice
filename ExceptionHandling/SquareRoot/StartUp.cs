97b7bb6fc6d84269c76975cf12956c057b9c5764
toshkotoshkov <toshkotoshkov@abv.bg>
2025-08-15 14:47:37 +0300

    Exception Handling First commit

---
+++ ExceptionHandling/SquareRoot/StartUp.cs
@@ -0,0 +1,32 @@
+﻿int number = 0;
+
+try
+{
+	number = int.Parse(Console.ReadLine());
+
+	if (number < 0)
+	{
+		throw new ArgumentException("The number cannot be a negative number");
+	}
+
+    Console.WriteLine(CalculateNumber(number));
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine("Please insert valid number!");
+
+    number = int.Parse(Console.ReadLine());
+
+    Console.WriteLine(CalculateNumber(number));
+}
+finally
+{
+    Console.WriteLine("Good bye");
+}
+
+static double CalculateNumber(int number)
+{
+    double squareRoot = Math.Sqrt((double)number);
+
+    return squareRoot;
+}
